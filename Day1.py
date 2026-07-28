75c97cab504428211191e472b3ee13259e32253a
akshaya234a <166231556+akshaya234a@users.noreply.github.com>
2026-07-28 13:18:41 +0530

    Tokens-Day1

---
+++ Day1.py
@@ -0,0 +1,27 @@
+print("Hello Codegnan")
+
+name="Akshaya"
+print(name)
+emailid = "burraakshaya51@gmail.com"
+print(emailid)
+
+email_id = "burraakshaya51@gmail.com"
+print(email_id)
+name="akshaya"
+print(name)
+
+batch="da23"
+print(batch)
+
+batch="cse"
+print(batch)
+
+length=5
+breadth=6
+area=length * breadth
+print(area)
+
+name,location,age="Akshaya","hyd",21
+print(name)
+print(location)
+print(age)
